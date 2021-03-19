dd2456abfb8a5daf0aca673bb188d74193a67f63
Daph <duong.huynh@aiesec.net>
2021-03-19 22:11:52 +0700

    Mar 19 10:11

--- main.py
+++ main.py
@@ -107,3 +107,28 @@ while True:
             i += 1
         except:
             continue
+
+'''
+AUD/CAD = 0
+AUD/JPY = 1
+AUD/NZD = 2
+AUD/USD = 3
+CAD/JPY = 4
+DAX = 5
+EUR/AUD = 6
+EUR/CAD = 7
+EUR/CHF = 8
+EUR/GBP = 9
+EUR/JPY = 10
+EUR/USD = 11
+GBP/AUD = 12
+GBP/CAD = 13
+GBP/CHF = 14 
+GBP/JPY = 15
+GBP/USD = 16
+NZD/USD = 17
+USD/CAD = 18
+USD/CHF = 19
+USD/JPY = 20
+XAU/USD = 21
+'''
